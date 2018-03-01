74bcdda1f918da9b46ec28ca4663e25a06a66dea
Takashi Kitajima <inc@2inc.org>
2018-03-01 10:20:19 +0900

    Fix #6

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -9,17 +9,20 @@ if [ ! -e .plugins ]; then
   rm -f bbpress.zip
 fi
 
-if [ ! -e .themes ]; then
-  GH=$(curl "https://api.github.com/repos/inc2734/snow-monkey/releases/latest");
-  SNOW_MONKEY=$(echo "$GH" | jq -r .assets[0].browser_download_url)
-
-  if [ -n "$SNOW_MONKEY" ]; then
-    curl -sL $SNOW_MONKEY -o snow-monkey.zip
-    mkdir -p .themes/snow-monkey
-    unzip -q snow-monkey.zip -d .themes/snow-monkey
-    rm -f snow-monkey.zip
-  else
-    echo $GH;
-    exit 1;
-  fi
-fi
+#
+# @todo It can not be used because it is caught by GitHub API rate limit...
+#
+# if [ ! -e .themes ]; then
+#   GH=$(curl "https://api.github.com/repos/inc2734/snow-monkey/releases/latest");
+#   SNOW_MONKEY=$(echo "$GH" | jq -r .assets[0].browser_download_url)
+#
+#   if [ -n "$SNOW_MONKEY" ]; then
+#     curl -sL $SNOW_MONKEY -o snow-monkey.zip
+#     mkdir -p .themes/snow-monkey
+#     unzip -q snow-monkey.zip -d .themes/snow-monkey
+#     rm -f snow-monkey.zip
+#   else
+#     echo $GH;
+#     exit 1;
+#   fi
+# fi
