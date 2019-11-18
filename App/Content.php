aaf99ab264cad672b918ae9fff135f36a8f27e24
Takashi Kitajima <inc@2inc.org>
2019-11-18 16:24:01 +0900

    Fix https://github.com/inc2734/snow-monkey/issues/668

--- App/Content.php
+++ App/Content.php
@@ -16,5 +16,22 @@ class Content {
 	public function _after_setup_theme() {
 		remove_filter( 'bbp_get_reply_content', 'bbp_make_clickable', 4 );
 		remove_filter( 'bbp_get_topic_content', 'bbp_make_clickable', 4 );
+		remove_filter( 'bbp_get_reply_content', 'bbp_make_clickable', 40 ); // v2.6.0〜
+		remove_filter( 'bbp_get_topic_content', 'bbp_make_clickable', 40 ); // v2.6.0〜
+
+		add_filter( 'bbp_get_reply_content', [ $this, '_wp_oembed_blog_card_sanitize' ], 100 );
+		add_filter( 'bbp_get_topic_content', [ $this, '_wp_oembed_blog_card_sanitize' ], 100 );
+	}
+
+	/**
+	 * @todo
+	 *
+	 * @param string $content
+	 * @return string
+	 */
+	public function _wp_oembed_blog_card_sanitize( $content ) {
+		$content = preg_replace( '|(<div class="wp-oembed-blog-card"[^>]+?><a [^>]+>)</p>|', '$1', $content );
+		$content = str_replace( '<p></a></div>', '</a></div>', $content );
+		return $content;
 	}
 }
