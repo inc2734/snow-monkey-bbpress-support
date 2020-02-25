479430855be6b1e36239fe1b72c7a0f2dec70769
Takashi Kitajima <inc@2inc.org>
2020-02-25 09:29:24 +0900

    0.7.2

--- snow-monkey-bbpress-support.php
+++ snow-monkey-bbpress-support.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin name: Snow Monkey bbPress Support
- * Version: 0.7.1
+ * Version: 0.7.2
  * Description: This plugin makes Snow Monkey beautifully display bbPress and adds some features.
  * Text Domain: snow-monkey-bbpress-support
  * Domain Path: /languages/
