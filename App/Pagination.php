e6faa33e63fbc4738f3de11f02904db75d043bba
Takashi Kitajima <inc@2inc.org>
2020-08-11 09:11:27 +0900

    fix #40

--- App/Pagination.php
+++ App/Pagination.php
@@ -10,9 +10,15 @@ namespace Snow_Monkey\Plugin\bbPressSupport\App;
 class Pagination {
 
 	public function __construct() {
+		add_filter( 'bbp_after_paginate_links_parse_args', [ $this, '_bbp_after_paginate_links_parse_args' ] );
 		add_filter( 'bbp_get_topic_pagination_links', [ $this, '_bbp_pagination_links' ] );
 		add_filter( 'bbp_get_search_pagination_links', [ $this, '_bbp_pagination_links' ] );
 		add_filter( 'bbp_get_forum_pagination_links', [ $this, '_bbp_pagination_links' ] );
+	}
+
+	public function _bbp_after_paginate_links_parse_args( $r ) {
+		$r['end_size'] = 1;
+		return $r;
 	}
 
 	public function _bbp_pagination_links( $pagination ) {
