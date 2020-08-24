aaa27704b8bae1bf2725a3ee4148c76fa8a75c8d
Takashi Kitajima <inc@2inc.org>
2020-08-24 15:22:28 +0900

    fix #45

--- App/Comments.php
+++ App/Comments.php
@@ -12,6 +12,7 @@ class Comments {
 	public function __construct() {
 		add_filter( 'comments_open', [ $this, '_comments_open' ], 10, 2 );
 		add_filter( 'pings_open', [ $this, '_comments_open' ], 10, 2 );
+		add_action( 'init', [ $this, '_remove_wp_make_content_images_responsive' ] );
 	}
 
 	public function _comments_open( $open, $post_id ) {
@@ -20,5 +21,12 @@ class Comments {
 		}
 
 		return false;
+	}
+
+	public function _remove_wp_make_content_images_responsive() {
+		remove_filter( 'bbp_get_reply_content', 'wp_make_content_images_responsive', 60 );
+		remove_filter( 'bbp_get_topic_content', 'wp_make_content_images_responsive', 60 );
+		add_filter( 'bbp_get_reply_content', 'wp_filter_content_tags', 60 );
+		add_filter( 'bbp_get_topic_content', 'wp_filter_content_tags', 60 );
 	}
 }
