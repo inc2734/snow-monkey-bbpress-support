9c643878816005c98c8ca16ff4fec4c089ccd9a7
inc2734 <inc@2inc.org>
2021-04-15 15:51:38 +0900

    fix #69

--- App/TopicCloseLink.php
+++ App/TopicCloseLink.php
@@ -13,24 +13,35 @@ class TopicCloseLink {
 	 * Constructor.
 	 */
 	public function __construct() {
-		add_action( 'bbp_template_before_replies_loop', [ $this, '_display_topic_close_link' ] );
+		add_action( 'bbp_template_before_replies_loop', [ $this, '_display_to_before_replies_loop' ] );
+		add_action( 'bbp_theme_after_reply_form_submit_button', [ $this, '_display_to_after_reply_form_submit_button' ] );
 	}
 
 	/**
-	 * Add topic close link
+	 * Return close link html.
 	 *
-	 * @return [void]
+	 * @return string
 	 */
-	public function _display_topic_close_link() {
+	protected function _close_link() {
 		$topic_id     = bbp_get_topic_id();
 		$topic        = bbp_get_topic( $topic_id );
 		$current_user = wp_get_current_user();
 
-		if ( empty( $topic->ID ) || ! current_user_can( 'participate', $topic->ID ) || (int) $current_user->ID !== (int) $topic->post_author ) {
+		if (
+			empty( $topic->ID )
+			|| ! current_user_can( 'participate', $topic->ID )
+			|| (int) $current_user->ID !== (int) $topic->post_author
+		) {
 			return;
 		}
 
-		if ( ! apply_filters( 'snow_monkey_bbpress_support_display_topic_close_link', '__return_true', $topic->ID ) ) {
+		if (
+			! apply_filters(
+				'snow_monkey_bbpress_support_display_topic_close_link',
+				'__return_true',
+				$topic->ID
+			)
+		) {
 			return;
 		}
 
@@ -51,10 +62,34 @@ class TopicCloseLink {
 			]
 		);
 		$uri     = wp_nonce_url( $uri, 'close-topic_' . $topic->ID );
+
+		return sprintf(
+			'<a href="%1$s" class="button">%2$s</a>',
+			esc_url( $uri ),
+			esc_html( $display )
+		);
+	}
+
+	/**
+	 * Add topic close link to before_replies_loop.
+	 */
+	public function _display_to_before_replies_loop() {
+		$close_link = $this->_close_link();
+		if ( ! $close_link ) {
+			return;
+		}
 		?>
 		<div class="snow-monkey-bbpress-support-my-topic-close-link">
-			<a href="<?php echo esc_url( $uri ); ?>" class="button"><?php echo esc_html( $display ); ?></a>
+			<?php echo wp_kses_post( $close_link ); ?>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Add topic close link to reply_form_submit_button.
+	 */
+	public function _display_to_after_reply_form_submit_button() {
+		$close_link = $this->_close_link();
+		echo wp_kses_post( $close_link );
 	}
 }
